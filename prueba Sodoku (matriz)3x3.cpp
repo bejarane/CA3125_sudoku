e28ebf7f1c298df8fb125c1b6b7c62271170249b
Keylor-D <72234326+Keylor-D@users.noreply.github.com>
2020-10-09 22:51:11 -0600

    Update prueba Sodoku (matriz)3x3.cpp

--- prueba Sodoku (matriz)3x3.cpp
+++ prueba Sodoku (matriz)3x3.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 using namespace std;
 
 int main () {
@@ -19,5 +19,34 @@ int main () {
 
 
 
+	return 0;
+}*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+	
+	int matriz [4][4],f,c;
+	cout << "digite el numero de filas: ";
+	cin >>f; 
+	cout << "digite el numero de  columnas: ";
+	cin >>c;
+	
+	for(int i=0;i<f;i++){
+		for(int j=0;j<c;j++){
+		cout << "introduzca los valores del sodoku ["<<i<<"]["<<j<<"]: ";
+		cin>> matriz[i][j];
+		}
+	}
+	for(int i=0;i<f;i++){
+		for(int j=0;j<c;j++){
+			cout << matriz[i][j];
+		}
+		cout<<"\n";
+	}
+	
+	
 	return 0;
 }
