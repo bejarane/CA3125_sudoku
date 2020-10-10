4512bc523e3754d8dc8a3aac22762e2e1c018a94
Keylor-D <72234326+Keylor-D@users.noreply.github.com>
2020-10-09 20:51:38 -0600

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,4 +1,7 @@
+#include <bits/stdc++.h>
 #include <clases.h>
+using namespace std;
+
 bool leerTipo(){
     string buffer;
     cin >> buffer;
@@ -43,5 +46,18 @@ int main(){
             casos --;
         }
     }
+    int matriz[3][3]; 
+	int f,c;
+	
+	for(f=0; f<3; f++){
+		for(c=0; c<3; c++){
+			matriz[f][c] = 1 + rand() %  9;
+		}
+	}
+	for(f=0; f<3; f++){
+		for(c=0; c<3; c++){
+			cout << matriz[f][c] <<" ";
+		}cout << endl;
+	}
     return 0;
 }
