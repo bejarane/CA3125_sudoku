aab4df2a7963613acdc69e16016bd3e94cd5bacb
Keylor-D <72234326+Keylor-D@users.noreply.github.com>
2020-10-09 22:51:20 -0600

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -46,18 +46,6 @@ int main(){
             casos --;
         }
     }
-    int matriz[3][3]; 
-	int f,c;
-	
-	for(f=0; f<3; f++){
-		for(c=0; c<3; c++){
-			matriz[f][c] = 1 + rand() %  9;
-		}
-	}
-	for(f=0; f<3; f++){
-		for(c=0; c<3; c++){
-			cout << matriz[f][c] <<" ";
-		}cout << endl;
-	}
+    
     return 0;
 }
